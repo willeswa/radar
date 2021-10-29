6826f8d829ce8ecaf582280c90c9d1a2041c405d
wanjala <gwiliez@gmail.com>
2021-10-29 23:09:01 +0300

    done with homescreen :)

--- app/src/main/java/com/wilies/radar/dailyweatherscreen/DailyWeatherFragment.kt
+++ app/src/main/java/com/wilies/radar/dailyweatherscreen/DailyWeatherFragment.kt
@@ -1,6 +1,7 @@
 package com.wilies.radar.dailyweatherscreen
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.wilies.radar.R
+import com.wilies.radar.adapters.WeatherRecyclerAdapter
 import com.wilies.radar.databinding.FragmentDailyWeatherBinding
 
 /**
@@ -17,6 +21,7 @@ import com.wilies.radar.databinding.FragmentDailyWeatherBinding
  */
 class DailyWeatherFragment : Fragment() {
     private val viewModel: DailyWeatherViewModel by viewModels()
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +33,15 @@ class DailyWeatherFragment : Fragment() {
             container,
             false)
 
+        val adapter = WeatherRecyclerAdapter()
+
+        binding.dailyweatherRecycler.adapter = adapter
+
         binding.lifecycleOwner = this
 
         binding.viewmodel = viewModel
+
+
 
         return binding.root
     }
--- app/src/main/java/com/wilies/radar/dailyweatherscreen/DailyWeatherViewModel.kt
+++ app/src/main/java/com/wilies/radar/dailyweatherscreen/DailyWeatherViewModel.kt
@@ -1,30 +1,53 @@
 package com.wilies.radar.dailyweatherscreen
 
+import android.app.Application
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.*
+import com.wilies.radar.R
 import com.wilies.radar.WeatherApi
 import com.wilies.radar.data.models.WeatherResponse
+import com.wilies.radar.utils.Utility
+import com.wilies.radar.utils.WeatherDescriptions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class DailyWeatherViewModel: ViewModel(){
+class DailyWeatherViewModel(application: Application) : AndroidViewModel(application) {
 
 
+    //hide the mutable response
     private val _response = MutableLiveData<WeatherResponse>()
 
-    private val dailyWeatherJob = Job()
+    val hourly_predictions = Transformations.map(_response){
+        it.hourly
+    }
 
-    private val coroutineScope = CoroutineScope(dailyWeatherJob + Dispatchers.Main)
-
+    //immutable response exposed to the views
     val response: LiveData<WeatherResponse>
         get() = _response
+
+
+    val currentPropertyDate = Transformations.map(response) {
+        Utility.getDateTime(it.current.dt)
+    }
+
+    val currentWeather = Transformations.map(response){
+        it.current.weather[0]
+    }
+
+    val tempInCelsius = Transformations.map(_response){
+        it.current.temp - 273.15
+    }
+
+
+
+    //a job to manage the coroutines
+    private val dailyWeatherJob = Job()
+
+    // a coroutine scope to handle the network execution
+    private val coroutineScope = CoroutineScope(dailyWeatherJob + Dispatchers.Main)
 
 
     init {
@@ -34,13 +57,11 @@ class DailyWeatherViewModel: ViewModel(){
     private fun getWeatherForecast() {
         coroutineScope.launch {
             var getWeatherDeferred = WeatherApi.retrofitService.getWeatherPredictions(
-                    0.660450, 28.761551, "API_KEY")
-
+                    0.660450, 28.761551, "21de641062c535eadf648e52e2a263c0")
             try {
-
                 _response.value = getWeatherDeferred
-            } catch (ex: Exception){
-                Log.i("TAG", ""+ex.message)
+            } catch (ex: Exception) {
+                Log.i("TAG", "" + ex.message)
             }
 
 
